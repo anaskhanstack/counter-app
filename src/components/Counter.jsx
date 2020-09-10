148b83b27f251ef48dc0c0b42bfd7164ba7aff7e
Muhammad Anas Khan <anas.khan6960@gmail.com>
2020-09-11 02:31:02 +0500

    added material ui

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,25 @@
 import React, { useState } from "react";
+import { Button, Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: "center",
+    fontSize: 19,
+  },
+}));
 
 const Counter = () => {
+  const classes = useStyles();
+
   const [counter, setCounter] = useState(0);
 
   const Increment = () => {
@@ -12,11 +31,34 @@ const Counter = () => {
   };
 
   return (
-    <div>
-      <h3>{counter < 1 ? "zero" : counter}</h3>
-      <button onClick={() => Increment()}>+</button>
-      <button onClick={() => Decrement()}>-</button>
-    </div>
+    <Grid direction="container">
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>
+          {counter < 1 ? "zero" : counter}
+        </Paper>
+      </Grid>
+
+      <Grid item xs={6}>
+        <div className={classes.root}>
+          <Button
+            onClick={() => Increment()}
+            variant="contained"
+            color="secondary"
+          >
+            Increment
+          </Button>
+          {counter < 0 ? (
+            <Button onClick={() => Decrement()} variant="contained" disabled>
+              Decrement
+            </Button>
+          ) : (
+            <Button onClick={() => Decrement()} variant="contained">
+              Decrement
+            </Button>
+          )}
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
